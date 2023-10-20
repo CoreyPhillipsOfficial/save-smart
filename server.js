0e1291dfc48d8a695ac520879b777cb1db08cb32
stasbaz <bazuluk989@gmail.com>
2023-10-20 13:55:52 -0600

    Merging front and back

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express");
-// const routes = require('./controllers');
+// // Import our view_routes
+// const { view_routes, user_routes } = require('./controllers');
 const db = require('./config/connection')
 const session = require('express-session');
 const { engine } = require("express-handlebars");
+
+// Create the port number and prepare for heroku with the process.env.PORT value
 const PORT = process.env.PORT || 3333;
 
+
+// Create the server app
 const app = express();
+
+
+const User = require('./models/User');
 
 
 // Set up Handlebars as the view engine
@@ -13,7 +21,11 @@ app.engine(".hbs", engine({extname: 'hbs'}))
 app.set("view engine", "hbs");
 app.set('views', './views')
 
+
+// Allow JSON to be sent from the client
 app.use(express.json())
+
+// Open the static channel for our browser assets - ie. express.static on the public folder
 app.use(express.static('public'));
 app.use(session({
     secret: 'Chunky cat',
@@ -22,8 +34,12 @@ app.use(session({
 }));
 
 
+// // Load our view routes at the root level '/'
+// app.use('/', view_routes);
 
-// app.use('/', routes)
+// // Load our user routes at the root level '/'
+// app.use('/auth', user_routes);
+
 
 db.sync().then(() => {
     app.listen(PORT, () => console.log("Server is running on port", PORT));
