53987fa1158920ff58336fe59f4e5bf44e2a395e
Rodolfo <rodolfoespinosa01@gmail.com>
2023-10-20 13:03:30 -0400

    Worked on setting up the server and creating a User Model Table

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+// Import the Sequelize constructor from sequelize
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+// Create a new connection instance, using option 3 from the docs
+const connection = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    // This is for the log in the terminal
+    logging: false,
+  }
+);
+
+
+
+// Export the connection object
+module.exports = connection;
---
+++ models/User.js
@@ -0,0 +1,51 @@
+// Import Model and DataTypes from sequelize
+const { Model, DataTypes } = require('sequelize');
+
+const db = require('../config/connection');
+
+// // If we use BCRYPT, bring in package
+// const { hash, compare } = require('bcrypt');
+
+// Create a User class and extend the Model class
+class User extends Model { }
+
+// Call User.init and setup a couple columns/fields - email & password as text strings
+User.init({
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: {
+      args: true,
+      msg: 'That email address is already in use.'
+    },
+    validate: {
+      isEmail: true
+    }
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: {
+        args: 6,
+        msg: 'Your password must be at least 6 characters in length.'
+      }
+    }
+  }
+}, {
+  modelName: 'user',
+  // Connection object
+  sequelize: db,
+
+  // // If we use BCRYPT use the code below
+  // hooks: {
+  //   async beforeCreate(user) {
+  //     user.password = await hash(user.password, 10);
+
+  //     return user;
+  //   }
+  // }
+});
+
+// Export the User model
+module.exports = User;
