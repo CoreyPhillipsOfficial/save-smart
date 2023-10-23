c00397fdecf5d829169b200d6b0a996c54074367
Rodolfo <rodolfoespinosa01@gmail.com>
2023-10-23 10:07:47 -0400

    Server is up and running, continuing initial setup

--- controllers/view_routes.js
+++ controllers/view_routes.js
@@ -1,4 +1,48 @@
-// Get route that res.render's the landing.hbs
+const router = require('express').Router();
 
-// Target your '/'
+const User = require('../models/User');
 
+
+// block an auth page if user is already logged in
+function isLoggedIn(req, res, next) {
+  if (req.session.user_id) {
+    return res.redirect('/')
+  }
+
+  next();
+}
+
+// block a route if a user is not logged in
+function isAuthenticated(req, res, next) {
+  if (!req.session.user_id) {
+    return res.redirect('/login')
+  }
+
+  next();
+}
+
+// attach user data to the request if they are logged in
+async function authenticate(req, res, next) {
+  const user_id = req.session.user_id;
+
+  if (user_id) {
+    const user = await User.findByPk(req.session.user_id, {
+      attributes: ['id', 'username']
+    });
+
+    req.user = user.get({ plain: true });
+
+  }
+
+  next();
+
+};
+
+// landing page
+router.get('/', authenticate, async (req, res) => {
+  
+  res.render('landing'); 
+});
+
+// Export the router
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
-// // Import our view_routes
-// const { view_routes, user_routes } = require('./controllers');
 const db = require('./config/connection')
-const session = require('express-session');
+const methodOverride = require('method-override');
+
 const { engine } = require("express-handlebars");
+
+const session = require('express-session');
+
+// Import View Routes
+const view_routes = require('./controllers/view_routes');
 
 // Create the port number and prepare for heroku with the process.env.PORT value
 const PORT = process.env.PORT || 3333;
@@ -12,35 +16,35 @@ const PORT = process.env.PORT || 3333;
 // Create the server app
 const app = express();
 
+// Open the static channel for our browser assets - ie. express.static on the public folder
+app.use(express.static('public'));
 
-const User = require('./models/User');
+// this is how we need to send it when using handlebars since it sends the data through urlencoding
+app.use(express.urlencoded({extended: false}));
+
+// Method Override Package to change Method in form
+app.use(methodOverride('_method'));
+
+// Load express-session middleware
+app.use(session({
+    secret: 'someSecretKey',
+    resave: false,
+    saveUninitialized: true
+  }))
+
+  // Load our view routes at the root level '/'
+app.use('/', view_routes);
 
 
 // Set up Handlebars as the view engine
-app.engine(".hbs", engine({extname: 'hbs'}))
-app.set("view engine", "hbs");
-app.set('views', './views')
+app.engine('.hbs', engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
 
 
-// Allow JSON to be sent from the client
-app.use(express.json())
 
-// Open the static channel for our browser assets - ie. express.static on the public folder
-app.use(express.static('public'));
-app.use(session({
-    secret: 'Chunky cat',
-    resave: false,
-    saveUninitialized: true
-}));
-
-
-// // Load our view routes at the root level '/'
-// app.use('/', view_routes);
-
-// // Load our user routes at the root level '/'
-// app.use('/auth', user_routes);
-
-
-db.sync().then(() => {
-    app.listen(PORT, () => console.log("Server is running on port", PORT));
-})
+// Sync and create tables
+// db.sync({force: false})
+// .then(() => {
+  // Start the server and log the port that it started on
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+// });
